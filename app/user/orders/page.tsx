277be7a20dc180411e27c7ca03ecf128f580e379
Toni Daada <tonidaada@gmail.com>
2025-11-12 02:41:02 +0100

    "Orders Pagination" 	modified:   app/user/orders/page.tsx 	new file:   components/shared/pagination.tsx 	modified:   lib/utils.ts 	modified:   package-lock.json 	modified:   package.json

--- app/user/orders/page.tsx
+++ app/user/orders/page.tsx
@@ -1,3 +1,4 @@
+import Pagination from "@/components/shared/pagination";
 import {
   Table,
   TableBody,
@@ -73,6 +74,12 @@ const OrdersPage = async (props: {
             ))}
           </TableBody>
         </Table>
+        {orders.totalPages >= 1 && (
+          <Pagination
+            page={Number(page) || 1}
+            totalPages={orders?.totalPages}
+          />
+        )}
       </div>
     </div>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,7 @@
 
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-
+import qs from "query-string";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -125,3 +125,27 @@ export const formatDateTime = (dateString: Date) => {
 // console.log(`FULL DATE TIME : ${formatted.dateTime}`);
 // console.log(`FULL DATE ONLY : ${formatted.dateOnly}`);
 // console.log(`FULL  TIME ONLY : ${formatted.timeOnly}`);
+
+// Form the pagination links
+export function formUrlQuery({
+  params,
+  key,
+  value,
+}: {
+  params: string;
+  key: string;
+  value: string | null;
+}) {
+  const query = qs.parse(params);
+  query[key] = value;
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query,
+    },
+    {
+      skipNull: true,
+    }
+  );
+}
