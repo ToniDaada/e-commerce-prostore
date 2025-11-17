f1ccd7aa8c7bde7e067c1193bece7680a37dcba8
Toni Daada <tonidaada@gmail.com>
2025-11-17 01:56:55 +0100

    "Protecting Admin Routes " 	modified:   app/admin/overview/page.tsx new file:   app/unauthorized/page.tsx 	new file:   lib/auth-guard.ts

--- app/admin/overview/page.tsx
+++ app/admin/overview/page.tsx
@@ -14,13 +14,16 @@ import { BadgeDollarSign, Barcode, CreditCard, Users } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
 import Charts from "./charts";
+import { requireAdmin } from "@/lib/auth-guard";
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
 };
 
 const AdminOverViewPage = async () => {
+  await requireAdmin();
   const session = await auth();
+
   if (session?.user.role !== "admin") throw new Error("User is not authorized");
 
   const summary = await getOrderSummary();
