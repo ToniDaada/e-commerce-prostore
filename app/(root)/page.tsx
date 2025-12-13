cfd0c488691e61caf70f7cc3989e2febf5c1792a
Toni Daada <tonidaada@gmail.com>
2025-12-14 00:56:50 +0100

    "Deal CountDow component" 	modified:   app/(root)/page.tsx 	new file:   components/deal-countdown.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import DealCountdown from "@/components/deal-countdown";
 import IconBoxes from "@/components/icon-boxes";
 import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
@@ -22,6 +23,7 @@ const HomePage = async () => {
       )}
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
       <ViewAllProductButton />
+      <DealCountdown />
       <IconBoxes />
     </>
   );
